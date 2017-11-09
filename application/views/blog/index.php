378ec3be49107ed65f31c1adae6e7261685ec4b9
Twotest2 <no@email.really>
2017-11-09 18:12:46 -0200

    first setup changes

---
+++ application/views/blog/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<title>Blog</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo
+        base_url('assets/css/bootstrap.min.css') ?>"/>
+</head>
+<body>
+    <h2>Hello</h2>
+</body>
+</html>
